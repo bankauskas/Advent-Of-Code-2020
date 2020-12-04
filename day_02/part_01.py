94597b546743ffab21da5a50b669846a3b4ec024
Arnoldas Bankauskas <banarn@swedwoodgroup.com>
2020-12-04 08:24:09 +0200

    Day 2 completed

---
+++ day_02/part_01.py
@@ -0,0 +1,17 @@
+import re
+
+validPasswords = []
+
+with open("input.txt") as f:
+    for line in f:
+        lowerBound = int(re.findall(r"^\d*",line)[0])
+        uperBound = int(re.findall(r"\-(\d*)\s",line)[0])
+        letter = re.findall(r"\s(\w*)\:",line)[0]
+        pasword = re.findall(r"\s(\w*)$",line)[0]
+
+        if  pasword.count(letter) >= lowerBound and pasword.count(letter) <= uperBound: 
+
+            validPasswords.append((lowerBound, uperBound, letter, pasword))
+
+if __name__ == '__main__':
+    print(len(validPasswords))
---
+++ day_02/part_02.py
@@ -0,0 +1,19 @@
+import re
+
+validPasswords = []
+
+with open("input.txt") as f:
+    for line in f:
+        lowerBound = int(re.findall(r"^\d*",line)[0])
+        uperBound = int(re.findall(r"\-(\d*)\s",line)[0])
+        letter = re.findall(r"\s(\w*)\:",line)[0]
+        pasword = re.findall(r"\s(\w*)$",line)[0]
+
+        # if  len(pasword) < uperBound:
+        #     print('{} {} {} {}'.format(lowerBound, uperBound, letter, pasword))
+        if  (pasword[lowerBound-1] == letter and pasword[uperBound-1] != letter) or (pasword[lowerBound-1] != letter and pasword[uperBound-1] == letter): 
+            validPasswords.append((lowerBound, uperBound, letter, pasword))
+
+
+if __name__ == '__main__':
+    print(len(validPasswords))
