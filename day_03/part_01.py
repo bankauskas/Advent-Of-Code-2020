1cabe57195ca6906eb76315eac209345734d11a0
Arnoldas Bankauskas <banarn@swedwoodgroup.com>
2020-12-05 23:28:23 +0200

    Day 3 part 1 (i think its shot be more simple way)

---
+++ day_03/part_01.py
@@ -0,0 +1,15 @@
+with open("day_03/input.txt") as f:
+    lists = f.read().splitlines()
+
+columns_count = len(lists[0])
+right, down = 3, 1
+row, column, trees = 0, 0, 0
+
+while row < len(lists):
+    if lists[row][column] == '#':
+        trees += 1
+    row += down
+    column = (column + right) % columns_count
+
+if __name__ == '__main__':
+    print(trees)
